46cc5be875039c779883790f2ac39f674dec63c4
Benny L.E.P <bendo01@gmail.com>
2026-07-25 12:48:46 +0800

    feat: add Individual master model and configure associations with reference entities

---
+++ server/src/models/person/master/individual.rs
@@ -0,0 +1,64 @@
+use sea_orm::entity::prelude::*;
+
+#[sea_orm::model]
+#[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
+#[sea_orm(schema_name = "person_master", table_name = "individuals")]
+
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: Uuid,
+    pub code: String,
+    pub name: String,
+    pub front_title: Option<String>,
+    pub last_title: Option<String>,
+    pub birth_date: Date,
+    pub birth_place: String,
+    #[sea_orm(default_value = "00000000-0000-0000-0000-000000000000")]
+    pub gender_id: Uuid,
+    #[sea_orm(default_value = "00000000-0000-0000-0000-000000000000")]
+    pub religion_id: Uuid,
+    #[sea_orm(default_value = "00000000-0000-0000-0000-000000000000")]
+    pub occupation_id: Uuid,
+    #[sea_orm(default_value = "00000000-0000-0000-0000-000000000000")]
+    pub education_id: Uuid,
+    #[sea_orm(default_value = "00000000-0000-0000-0000-000000000000")]
+    pub income_id: Uuid,
+    #[sea_orm(default_value = "00000000-0000-0000-0000-000000000000")]
+    pub identification_type_id: Uuid,
+    #[sea_orm(default_value = "00000000-0000-0000-0000-000000000000")]
+    pub marital_status_id: Uuid,
+    #[sea_orm(default_value = "00000000-0000-0000-0000-000000000000")]
+    pub profession_id: Uuid,
+    #[sea_orm(default_value = "00000000-0000-0000-0000-000000000000")]
+    pub age_classification_id: Uuid,
+    #[sea_orm(default_value = false)]
+    pub is_special_need: bool,
+    #[sea_orm(default_value = false)]
+    pub is_social_protection_card_recipient: bool,
+    #[sea_orm(default_value = false)]
+    pub is_deceased: bool,
+    pub created_at: Option<DateTime>,
+    pub updated_at: Option<DateTime>,
+    pub deleted_at: Option<DateTime>,
+    pub sync_at: Option<DateTime>,
+    pub created_by: Option<Uuid>,
+    pub updated_by: Option<Uuid>,
+    #[sea_orm(belongs_to, from = "gender_id", to = "id")]
+    pub gender: BelongsTo<crate::models::person::reference::gender::Entity>,
+    #[sea_orm(belongs_to, from = "religion_id", to = "id")]
+    pub religion: BelongsTo<crate::models::person::reference::religion::Entity>,
+    #[sea_orm(belongs_to, from = "occupation_id", to = "id")]
+    pub occupation: BelongsTo<crate::models::person::reference::occupation::Entity>,
+    #[sea_orm(belongs_to, from = "income_id", to = "id")]
+    pub income: BelongsTo<crate::models::person::reference::income::Entity>,
+    #[sea_orm(belongs_to, from = "identification_type_id", to = "id")]
+    pub identification_type: BelongsTo<crate::models::person::reference::identification_type::Entity>,
+    #[sea_orm(belongs_to, from = "marital_status_id", to = "id")]
+    pub marital_status: BelongsTo<crate::models::person::reference::marital_status::Entity>,
+    #[sea_orm(belongs_to, from = "profession_id", to = "id")]
+    pub profession: BelongsTo<crate::models::person::reference::profession::Entity>,
+    #[sea_orm(belongs_to, from = "age_classification_id", to = "id")]
+    pub age_classification: BelongsTo<crate::models::person::reference::age_classification::Entity>,
+}
+
+impl ActiveModelBehavior for ActiveModel {}
--- server/src/models/person/reference/occupation.rs
+++ server/src/models/person/reference/occupation.rs
@@ -17,6 +17,8 @@ pub struct Model {
     pub sync_at: Option<DateTime>,
     pub created_by: Option<Uuid>,
     pub updated_by: Option<Uuid>,
+    #[sea_orm(has_many)]
+    pub individuals: HasMany<crate::models::person::master::individual::Entity>,
 }
 
 impl ActiveModelBehavior for ActiveModel {}
