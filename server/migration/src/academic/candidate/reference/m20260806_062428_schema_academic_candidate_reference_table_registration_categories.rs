2e05faf9b35bbb9d8095d2391165a2bdc18afe3c
Benny L.E.P <bendo01@gmail.com>
2026-08-06 14:45:15 +0800

    feat: initialize academic schema migrations for campaign, candidate, and course modules

---
+++ server/migration/src/academic/candidate/reference/m20260806_062428_schema_academic_candidate_reference_table_registration_categories.rs
@@ -0,0 +1,22 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+pub struct Migration;
+
+impl MigrationName for Migration {
+    fn name(&self) -> &str {
+        "m20260806_062428_schema_academic_candidate_reference_table_registration_categories"
+    }
+}
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        todo!();
+    }
+
+    async fn down(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        todo!();
+    }
+}
